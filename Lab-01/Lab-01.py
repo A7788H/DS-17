0d6395742518a65c598e4b0ad5c057d59bf55719
A7788H <124714622+A7788H@users.noreply.github.com>
2023-02-11 22:37:16 +0200

    11.02

--- Lab-01/Lab-01.py
+++ Lab-01/Lab-01.py
@@ -1,1 +1,32 @@
-print("helloooooo")
+# -*- coding: utf-8 -*-
+
+# -- Sheet --
+
+# ## 1. Markdown cells ##
+# 
+# * Hi! its Maine Academy?
+# * All HTML tabs are supported!
+# * Моя сторінка на [Linkedln](https://www.linkedin.com/in/antony-ganus/)
+# 
+# # This is Heading1 #
+# 
+# ## This is Heading2 ##
+# 
+# <span style ="color:grey">This is where we can bring qoutes and present them nicely</span>
+# 
+# Math:  
+#         
+# $$y_{sub}={\sqrt({x_{sub}})}$$
+# 
+#         
+# Or python code exampel
+# 
+# ```
+# import this
+#  def foo()
+#     return "foo
+# ```
+
+
+
+
---
+++ Lab-01/additional task.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# -- Sheet --
+
+# Here is the sample mean:
+#  $$
+#  y = {\frac{1}{2}}{\sum\limits_{i=1}^n}{y_1}$$
+#         
+# Here is the sample variance:
+# $$ 
+#    \sigma^2  =  {{\sum\limits_{i=1}^n}{(y_i-{\bar{y}})^2}\over{n-1}}
+#         $$
+# 
+# Here is the sample standart deviation:
+# $$
+#     \sigma = {\sqrt{{\sum\limits_{i=1}^n}{(y_i-{\bar{y}})^2}\over{n-1}}}
+#         $$
+
+
+
+
